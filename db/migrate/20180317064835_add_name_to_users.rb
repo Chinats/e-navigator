260a00f8d56ee17f4e515465843bfab101f78e36
kanayama <knym.777@gmail.com>
2018-03-18 10:40:51 +0900

    modify: setting enum & data_field

--- db/migrate/20180317064835_add_name_to_users.rb
+++ db/migrate/20180317064835_add_name_to_users.rb
@@ -2,7 +2,7 @@ class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :name, :string
     add_column :users, :birthday, :datetime
-    add_column :users, :sex, :integer
+    add_column :users, :sex, :integer,  limit: 1, default:nil
     add_column :users, :school, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180317064835) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.datetime "birthday"
-    t.integer "sex"
+    t.integer "sex", limit: 2
     t.string "school"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
