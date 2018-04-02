d8d251361a3c92e37fb7b39ffcf4d4c0b3fa89bd
kanayama <knym.777@gmail.com>
2018-04-02 12:55:02 +0900

    自分以外のユーザーの一覧をトップページに表示: 表示内容を整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum sex: { male: 0, female: 1, others: 2 }
+  enum sex: { 男性: 0, 女性: 1, その他: 2 }
   has_many :interviews, dependent: :destroy
 end
