4f54c6399a9697c36154fa80e025ad078942f750
kanayama <knym.777@gmail.com>
2018-04-06 11:49:48 +0900

    面接官用の面接日程一覧ページを作る: ユーザーIDに応じて面談一覧のリンクを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :interviews
   devise_for :users
+  resources :users do
+    get 'interviews/index'
+  end
   root 'users#index'
   get 'users/index'
 end
