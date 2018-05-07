88841af68864a38c0d617a404c41f4e79b32fc66
kanayama <knym.777@gmail.com>
2018-05-08 08:12:15 +0900

    面談日程を知らせるためのメール送信機能: production用のmailerを追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Mailer setup
+  config.action_mailer.perform_deliveries = true
+  mail = ENV['MAIL_ADDRESS']
+  pass = ENV['MAIL_PASSWORD']
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            mail,
+    password:             pass,
+    authentication:       'login',
+    enable_starttls_auto: true  }
 end
