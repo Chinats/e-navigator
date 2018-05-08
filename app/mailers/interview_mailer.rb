5c6d06fb6e47531a8df5e20a2e86a264df71c8fb
Kanayama Chinatsu <c.kanayama@everyleaf.com>
2018-05-08 20:20:49 +0900

    面接日程を知らせるためのメール送信機能: 送信するメールの内容を修正

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -3,7 +3,9 @@ class InterviewMailer < ApplicationMailer
 
   def interview_email(user)
     @user = user
+    @current_user = current_user
     @url  = 'https://e-navigator-chinats.herokuapp.com/'
-    mail(to: @user.email, subject: '【e-navigator】面談希望日が決定しました。')
+    @interview = Interview.approved.find_by(user_id: user.id)&.candidate
+    mail(to: @user.email, subject: '【e-navigator】面談日時が確定しました。')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'heroku.com',
     user_name:            mail,
     password:             pass,
     authentication:       :plain,
