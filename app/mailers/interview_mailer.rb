7a41d963c64d2c1d0a5aecd793b7253a54e9a7a0
Kanayama Chinatsu <c.kanayama@everyleaf.com>
2018-05-15 19:12:59 +0900

    レビュー修正: 不要な記述を削除

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -4,7 +4,6 @@ class InterviewMailer < ApplicationMailer
   def interview_email(user, current_user)
     @user = user
     @current_user = current_user
-    @url  = 'https://e-navigator-chinats.herokuapp.com/'
     @interview = Interview.approved.find_by(user_id: user.id)&.candidate
     mail(to: @user.email, subject: '【e-navigator】面談日時が確定しました。')
   end
